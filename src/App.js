9384f4439053bf5c4fdbd14dc955ace630d84458
Erin <se.young492@gmail.com>
2021-11-02 19:19:36 +0900

    seyoung join

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import React from "react";
 
+class App extends React.Component{
+  join = () => {
+    console.log('join');
+  }
+
+  render(){
+    
+    return (
+      <div className="App">
+        <h2>Join / Login</h2>
+        <button onClick={this.join}>Join</button>
+      </div>
+    )
+  }
+}
 export default App;
